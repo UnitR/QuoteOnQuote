46ab2b3c230ce13131ce6d6e9f2469f63b3d4705
Angel Peltekov <ai.peltekov@gmail.com>
2017-06-10 15:29:00 +0300

    Different projects for different purposes

---
+++ QuoteOnQuote.Data/Data/QuoteOnQuoteDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace QuoteOnQuote.Data.Data
+{
+    public class QuoteOnQuoteDbContext : DbContext
+    {
+
+    }
+}
---
+++ QuoteOnQuote.Models/Votes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuoteOnQuote.Models
+{
+    class Votes
+    {
+        public int Score { get; set; }
+
+        public int QuoteId { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
