df6f83df841b01c63776916a5f4f5e1de8df5107
Angel Peltekov <ai.peltekov@gmail.com>
2017-06-12 16:29:01 +0300

    AJAX nugget packages

--- QuoteOnQuote.Models/Votes.cs
+++ QuoteOnQuote.Models/Votes.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace Data.Models
+﻿namespace Data.Models
 {
     public class Votes
     {
