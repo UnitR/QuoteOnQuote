b9f1befe83d8095dd6363eb482406b6dd933cbc2
Angel Peltekov <ai.peltekov@gmail.com>
2017-06-15 01:30:16 +0300

    Restricted website to logged in users only

--- QuoteOnQuote/Controllers/AccountController.cs
+++ QuoteOnQuote/Controllers/AccountController.cs
@@ -58,6 +58,11 @@ namespace QuoteOnQuote.Controllers
         [AllowAnonymous]
         public ActionResult Login(string returnUrl)
         {
+            if (Request.IsAuthenticated)
+            {
+                return RedirectToAction("Dashboard", "Home");
+            }
+
             ViewBag.ReturnUrl = returnUrl;
             return View();
         }
@@ -393,7 +398,7 @@ namespace QuoteOnQuote.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
         }
 
         //
