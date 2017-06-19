b9a882e39fc75b2ca89e00be2448026f13872502
Angel Peltekov <ai.peltekov@gmail.com>
2017-06-19 12:03:39 +0300

    Connected table Votes with Quotes

--- QuoteOnQuote.Data/Migrations/Configuration.cs
+++ QuoteOnQuote.Data/Migrations/Configuration.cs
@@ -1,10 +1,7 @@
-namespace Data.Migrations
-{
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
+using System.Data.Entity.Migrations;
 
+namespace QuoteOnQuote.Data.Migrations
+{
     internal sealed class Configuration : DbMigrationsConfiguration<Data.QuoteOnQuoteDbContext>
     {
         public Configuration()
